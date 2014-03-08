d1fb16e288d8ab53729a1649f3532ef173df2358
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-03-08 12:53:11 +0100

    adjust tests.

--- test/ritual/core_test.clj
+++ test/ritual/core_test.clj
@@ -3,23 +3,10 @@
             [ritual.core :refer :all]
             [ritual.table :refer [drop-if-exists!]]
             [ritual.db :refer :all]
-            [clojure.java.jdbc :as db]))
+            [clojure.java.jdbc :as db]
+            [pandect.core :refer [sha1]]))
 
 ;; ## Fixture Helpers
-
-(def people
-  (table
-    [{:id 1234 :name "Me"}
-     {:id 5678 :name "You" :address "Here"}]
-    :primary-key :id))
-
-(defn people!
-  [db-atom & args]
-  (swap! db-atom #(apply people % args)))
-
-(defn cleanup!
-  [db-atom]
-  (swap! db-atom cleanup))
 
 (defn query-count!
   [db-atom table]
@@ -30,78 +17,115 @@
   (db/query @db-atom (str "select * from " table)
             :row-fn (juxt :id :name :address)))
 
-;; ## Tests
+;; ## Basic Tests
 
-(let [db (atom (create-derby :cores))]
-  (drop-if-exists! @db :people)
-
-  (fact "about the initial database state."
-        (query-count! db "people") => (throws Exception #"does not exist"))
-
-  (fact "about creating a table."
-        (people! db :people :insert? false) => truthy
-        (query-count! db "people") => 0
-        (people! db :people :insert? false) => truthy
-        (query-count! db "people") => 0
-        (cleanup! db) => truthy
-        (query-count! db "people") => (throws Exception #"does not exist"))
-
-  (fact "about creating/inserting data."
-        (people! db :people) => truthy
-        (query-count! db "people") => 2
-        (query-data! db "people") => (contains #{[1234 "Me" nil] [5678 "You" "Here"]})
-        (cleanup! db) => truthy
-        (query-count! db "people") => (throws Exception #"does not exist"))
-
-  (fact "about creating/inserting data into multiple tables."
-        (swap! db #(-> % (people :people) (people :other-people))) => truthy
-        (query-count! db "people") => 2
-        (query-count! db "other_people") => 2
-        (query-data! db "people") => (query-data! db "other_people")
-        (cleanup! db) => truthy
-        (query-count! db "people") => (throws Exception #"does not exist")
-        (query-count! db "other_people") => (throws Exception #"does not exist"))
-
-  (with-state-changes [(before :facts (people! db :people :insert? false))
-                       (after :facts (cleanup! db))]
-    (fact "about insert without creating/dropping a table"
-          (let [db' (people @db :people :force? false :cleanup :all)]
-            db' => @db
-            (query-count! db "people") => 2
-            (people @db :people :force? false) => (throws Exception #"duplicate key value")
-            (query-count! db "people") => 2
-            (cleanup db') => db'
-            (query-count! db "people") => 0)))
-
-  (with-state-changes [(before :facts (people! db :people))
-                       (after :facts (cleanup! db))]
-    (fact "about snapshots/dump based on DB spec."
-          (let [s (snapshot @db :people)]
-            s => map?
-            (count s) => 2
-            (keys s) => (contains #{1234 5678}))
-          (dump @db :people) => {1234 {:id 1234 :name "Me"  :address nil}
-                                 5678 {:id 5678 :name "You" :address "Here"}})))
-
-;; ## Tests for Cleanup
-
-(def people-for-cleanup
+(def people
   (table
     [{:id 1234 :name "Me"}
      {:id 5678 :name "You" :address "Here"}]
-    :primary-key :id
-    :cleanup-by [:id]))
+    :primary-key :id))
 
-(let [db (atom (create-derby :core-cleanup))]
-  (drop-if-exists! @db :people)
-  (fact "about selective cleanup."
-        (swap! db people-for-cleanup :people) => truthy
-        (db/insert! @db :people [:id] [90]) => [1]
-        (query-count! db "people") => 3
-        (query-data! db "people") => (contains #{[1234 "Me" nil] [5678 "You" "Here"] [90 nil nil]})
-        (cleanup! db) => truthy
-        (query-count! db "people") => 1
-        (query-data! db "people") => (contains #{[90 nil nil]})))
+(let [db* (create-derby :core)
+      db (atom nil)]
+  (with-state-changes [(before :facts (do
+                                        (reset! db db*)
+                                        (drop-if-exists! @db :people)
+                                        (drop-if-exists! @db :other-people))) ]
+    (fact "about the initial database state."
+          (query-count! db "people") => (throws Exception #"does not exist"))
+
+    (fact "about creating/dropping a table."
+          (swap! db people "people" :insert? false) => truthy
+          (query-count! db "people") => 0
+          (swap! db people "people" :insert? false) => truthy
+          (query-count! db "people") => 0
+          (swap! db cleanup) => truthy
+          (query-count! db "people") => (throws Exception #"does not exist"))
+
+    (fact "about creating/inserting data."
+          (swap! db people "people") => truthy
+          (query-count! db "people") => 2
+          (query-data! db "people") => (contains #{[1234 "Me" nil] [5678 "You" "Here"]})
+          (swap! db cleanup) => truthy
+          (query-count! db "people") => (throws Exception #"does not exist"))
+
+    (fact "about creating/inserting data into multiple tables."
+          (swap! db #(-> % (people :people) (people :other-people))) => truthy
+          (query-count! db "people") => 2
+          (query-count! db "other_people") => 2
+          (query-data! db "people") => (query-data! db "other_people")
+          (swap! db cleanup) => truthy
+          (query-count! db "people") => (throws Exception #"does not exist")
+          (query-count! db "other_people") => (throws Exception #"does not exist"))
+
+    (fact "about creating a table only if it doesn't exist."
+          (swap! db people "people") => truthy
+          (swap! db people "people" :insert? false) => truthy
+          (query-count! db "people") => 2
+          (swap! db people "people") => (throws Exception #"duplicate key value"))
+
+    (fact "about forcing the creation of a table (and thus deleting all existing data)."
+          (swap! db people "people") => truthy
+          (swap! db people "people" :insert? false :force? true) => truthy
+          (query-count! db "people") => 0
+          (swap! db people "people") => truthy
+          (query-count! db "people") => 2)
+
+    (facts "about cleanup via drop."
+           (fact "default behaviour"
+             (swap! db people "people") => truthy
+             (swap! db cleanup) => truthy
+             (query-count! db "people") => (throws Exception #"does not exist"))
+           (fact "explicit `:drop`"
+             (swap! db people "people" :cleanup :drop) => truthy
+             (swap! db cleanup) => truthy
+             (query-count! db "people") => (throws Exception #"does not exist")))
+
+    (fact "about suppressing cleanup."
+          (swap! db people "people" :cleanup :none) => truthy
+          (swap! db cleanup) => truthy
+          (query-count! db "people") => 2)
+
+    (fact "about non-dropping cleanup."
+          (swap! db people "people" :cleanup :clear) => truthy
+          (swap! db cleanup) => truthy
+          (query-count! db "people") => 0)
+
+    (facts "about selective cleanup."
+           (fact "about cleaning up initial data."
+             (swap! db people "people" :cleanup :id) => truthy
+             (db/insert! @db :people [:id] [90]) => [1]
+             (query-count! db "people") => 3
+             (swap! db cleanup) => truthy
+             (query-count! db "people") => 1
+             (query-data! db "people") => [[90 nil nil]])
+           (fact "about cleaning up data by some column."
+             (swap! db people "people" :cleanup :name) => truthy
+             (db/insert! @db :people [:id :name] [90 "Me"] [121 "You"] [234 "Him"]) => [1 1 1]
+             (query-count! db "people") => 5
+             (swap! db cleanup) => truthy
+             (query-count! db "people") => 1
+             (query-data! db "people") => [[234 "Him" nil]])))
+
+  (tabular
+    (fact "about snapshots based on DB spec."
+          (drop-if-exists! @db :people)
+          (swap! db people "people") => truthy
+          (let [s (apply snapshot @db :people ?args)
+                k (set ?keys)]
+            s => map?
+            (count s) => (count k)
+            (keys s) => (contains k)
+            s => (contains ?part)))
+    ?args               ?keys            ?part
+    []                  [1234 5678]      {}
+    [:by :id]           [1234 5678]      {}
+    [:by :name]         ["Me" "You"]     {}
+    [:by :address]      ["Here"]         {}
+    [:only [:id]]       [1234 5678]      {1234 (sha1 "1234") 5678 (sha1 "5678")}
+    [:only [:name]]     [1234 5678]      {1234 (sha1 "\"Me\"") 5678 (sha1 "\"You\"")}
+    [:only [:id :name]] [1234 5678]      {1234 (sha1 "1234,\"Me\"") 5678 (sha1 "5678,\"You\"")}
+    [:only [:name :id]] [1234 5678]      {1234 (sha1 "1234,\"Me\"") 5678 (sha1 "5678,\"You\"")}) )
 
 ;; ## Tests for Overrides
 
